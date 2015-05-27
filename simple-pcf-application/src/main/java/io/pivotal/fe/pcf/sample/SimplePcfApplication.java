8ee98e74c25cc6f66e2cb806901fa9783c710eb9
lshannon <luke.shannon@gmail.com>
2015-05-27 00:09:52 -0400

    First Check In

---
+++ simple-pcf-application/src/main/java/io/pivotal/fe/pcf/sample/SimplePcfApplication.java
@@ -0,0 +1,12 @@
+package io.pivotal.fe.pcf.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplePcfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplePcfApplication.class, args);
+    }
+}
---
+++ simple-pcf-application/src/main/java/io/pivotal/fe/pcf/sample/controllers/BasicController.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package io.pivotal.fe.pcf.sample.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Simple Controller for testing to ensure an end point works
+ * @author lshannon
+ *
+ */
+@Controller
+public class BasicController {
+	
+	@RequestMapping(method=RequestMethod.GET)
+    public @ResponseBody String helloMeetUp() {
+        return "Hello Toronto Pivotal User Group";
+    }
+
+}
