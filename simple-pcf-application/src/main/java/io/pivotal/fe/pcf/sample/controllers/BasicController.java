6cd245b8bd72d30d80e69f0219c0827e072f2bed
lshannonPivotal <lshannon@pivotal.io>
2016-01-18 10:17:57 -0500

    Tangerine meeting

--- simple-pcf-application/src/main/java/io/pivotal/fe/pcf/sample/controllers/BasicController.java
+++ simple-pcf-application/src/main/java/io/pivotal/fe/pcf/sample/controllers/BasicController.java
@@ -18,7 +18,7 @@ public class BasicController {
 	
 	@RequestMapping(method=RequestMethod.GET)
     public @ResponseBody String helloMeetUp() {
-        return "Hello TD Friends!";
+        return "Hello Tangerine Friends!";
     }
 
 }
