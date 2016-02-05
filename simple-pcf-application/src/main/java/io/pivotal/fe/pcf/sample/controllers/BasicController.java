9cdd698a4a3546e54903cd5329c3ec6ea4f50d7b
lshannonPivotal <lshannon@pivotal.io>
2016-02-05 10:47:50 -0500

    CICB demo

--- simple-pcf-application/src/main/java/io/pivotal/fe/pcf/sample/controllers/BasicController.java
+++ simple-pcf-application/src/main/java/io/pivotal/fe/pcf/sample/controllers/BasicController.java
@@ -18,7 +18,7 @@ public class BasicController {
 	
 	@RequestMapping(method=RequestMethod.GET)
     public @ResponseBody String helloMeetUp() {
-        return "Hello OTPP Friends! How Long to detect??";
+        return "Hello CIBC Friends! How Long to detect??";
     }
 
 }
