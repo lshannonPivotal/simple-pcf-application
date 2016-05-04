8c49fd03702a20288c7dc7efbdb491813313dcd0
lshannon <luke.shannon@gmail.com>
2016-05-04 10:40:28 -0400

    Demo for Adam

--- simple-pcf-application/src/main/java/io/pivotal/fe/pcf/sample/controllers/BasicController.java
+++ simple-pcf-application/src/main/java/io/pivotal/fe/pcf/sample/controllers/BasicController.java
@@ -18,7 +18,7 @@ public class BasicController {
 
 	@RequestMapping(method=RequestMethod.GET)
     public @ResponseBody String helloMeetUp() {
-        return "Hello Telus - delivered from Jenkins! Thanks Aron!";
+        return "Hello Adam - delivered from Jenkins! Thanks Adam!";
     }
 
 }
